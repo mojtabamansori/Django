b341e1f4b91193c802f0516a95cd00c7cde84394
mojtabamansori <13mrlien13@gmail.com>
2024-05-26 21:51:22 -0700

    video 11

---
+++ course_lg/4/test_1/challenges/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+days = {
+    'saturday' : 'this is saturday in disctionary',
+    'sunday'   : 'this is sunday in disctionary',
+    'monday'   : 'this is monday in disctionary',
+    'tuesday'  : 'this is tuesday in disctionary',
+    'wednesday': 'this is wednesday in disctionary',
+    'thursday': 'this is thursday in disctionary',
+    'friday'   : 'this is friday in disctionary',
+}
+
+def dynamic_days_by_number(request, day):
+    days_names = list(days.keys())
+    if day > len(days_names):
+        return HttpResponseNotFound('day does not exists')
+
+    redirect_day = days_names[day - 1]
+    redirect_url = reverse('days-of-week',args=[redirect_day])
+    return HttpResponseRedirect(redirect_url)
+    # return HttpResponse(day)
+def dynamic_days(request, day):
+    day_data = days.get(day)
+    if day_data is not None:
+        return HttpResponse(f'day is : {day} and data is {day_data}')
+
+    return HttpResponseNotFound('day does not exists')
+
