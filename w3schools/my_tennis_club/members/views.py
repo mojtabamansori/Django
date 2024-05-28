bfb326686902e6cb56e7baa02a796e411e889896
mojtabamansori <13mrlien13@gmail.com>
2024-05-28 08:45:13 -0700

    update w3schppl

--- w3schools/my_tennis_club/members/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
---
+++ w3schools/test/my_tennis_club/members/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
